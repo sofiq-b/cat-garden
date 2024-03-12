2136c6d204d0ed34c9184d688ab849145772c71d
Sophia <sophiaalbenova@gmail.com>
2024-03-13 01:52:34 +0200

    Add project files.

---
+++ CatGarden/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CatGarden.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
