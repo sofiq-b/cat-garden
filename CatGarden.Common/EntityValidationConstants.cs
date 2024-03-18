147f79ea53f0ce6bf74371fe0fc19ad4f531ba38
Sophia <sophiaalbenova@gmail.com>
2024-03-19 01:10:09 +0200

    Added Article model

---
+++ CatGarden.Common/EntityValidationConstants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatGarden.Common
+{
+    public static class EntityValidationConstants
+    {
+        public static class Article
+        {
+            public const int TitleMinLength = 5;
+            public const int TitleMaxLength = 120;
+
+            public const int ContentMinLength = 500;
+            public const int ContentMaxLength = 2000;
+        }
+    }
+}
