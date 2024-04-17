b771ba4778b3e71b80ae84a0d65ba821fba3738b
Sophia <sophiaalbenova@gmail.com>
2024-04-17 21:12:36 +0300

    Remove IImageService unused methods

--- CatGarden.Services.Data/Interfaces/IImageService.cs
+++ CatGarden.Services.Data/Interfaces/IImageService.cs
@@ -9,10 +9,8 @@ namespace CatGarden.Services.Data.Interfaces
         Task<string> UploadImageAsync(string folderPath, IFormFile file );
         Task<string> EditImageAsync(string folderPath, int imageId, IFormFile newFile);
         bool DeleteImageByNameAndCatId(string imageName, int catId);
-        Task UpdateImageAsync(Image image);
         Task<int?> FindImageIdByFileNameAsync(string fileUrl);
         Task<List<ImageModel>> GetCatImagesAsync(Cat cat);
         Task<List<ImageModel>> GetCatteryImagesAsync(Cattery cattery);
-        Task DeleteImageAsync(int imageId);
     }
 }
